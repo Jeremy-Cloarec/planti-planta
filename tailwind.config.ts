f12e86ce3a04c61920b07f5cbb598687e7ae5762
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-01-22 20:14:05 +0100

    style: add configurations, colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,15 @@ export default {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        dark: "var(--dark)",
+        green: "var(--green)",
+        red: "var(--red)",
+        white: "var(--white)",
       },
+      fontFamily: {
+        display: "var(--display)",
+        sans: "var(--inter)",
+      }
     },
   },
   plugins: [],
