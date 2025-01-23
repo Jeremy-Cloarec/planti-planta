01d3178c9f4cdb4f41d6e0b44f4c4440729d34da
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-01-23 19:57:40 +0100

    fix:fetch in poduction

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,15 +1,13 @@
 import { sql } from "@vercel/postgres";
-import { Plants } from "./definitions";
+
 
 export async function fetchPlants() {
     try {
         const data = await sql`SELECT * FROM plants`;
         return data.rows;
 
-    } catch (error:any) {
+    } catch (error) {
         console.error('Database Error:', error);
-        console.error('Error name:', error.name);
-        console.error('Error message:', error.message);
         throw new Error('Failed to fetch revenue data.');
     }
 }
