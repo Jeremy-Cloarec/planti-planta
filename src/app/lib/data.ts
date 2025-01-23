ae0a837a6162c1d5cfedaec13e2067ef7aca4ef8
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-01-23 19:34:45 +0100

    fix:fetch in poduction

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -3,7 +3,7 @@ import { Plants } from "./definitions";
 
 export async function fetchPlants() {
     try {
-        const data = await sql<Plants>`SELECT * FROM plants`;
+        const data = await sql<Plants>`SELECT * FROM public.plants`;
         return data.rows;
 
     } catch (error) {
