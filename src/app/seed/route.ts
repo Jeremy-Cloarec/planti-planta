4b68c24f6f7cc86ccffb0c65f95a7ab0a3204983
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-01-24 18:08:06 +0100

    fix: fetch in poduction

---
+++ src/app/seed/route.ts
@@ -0,0 +1,46 @@
+import { db } from "@vercel/postgres";
+
+const client = await db.connect();
+
+async function seedPlants() {
+    try {
+        await client.sql`DROP TABLE IF EXISTS plants`; 
+        await client.sql`
+        CREATE TABLE IF NOT EXISTS plants (
+            id SERIAL PRIMARY KEY,
+            title TEXT NOT NULL,
+            price NUMERIC NOT NULL,
+            shop BOOLEAN NOT NULL
+        )
+    `;
+        await client.sql`
+        INSERT INTO plants (title, price, shop) VALUES
+            ('Iridaceae', 16, FALSE),
+            ('Ericaceae', 24, FALSE),
+            ('Geraniaceae', 8, FALSE),
+            ('Aizoaceae', 12, FALSE),
+            ('Liliaceae', 31, FALSE),
+            ('Campanulaceae', 24, FALSE)
+            ON CONFLICT DO NOTHING;
+    `;
+        console.log('Database seeding successful');
+        Response.json({ message: 'Database seeded successfully' });
+
+    } catch (error) {
+        throw new Error(`Database seeding failed: ${error}`);
+    }
+}
+
+// Fonction GET pour initialiser la base de données
+export async function GET() {
+    try {
+        await client.sql`BEGIN`;
+        await seedPlants();
+        await client.sql`COMMIT`;
+        return Response.json({ message: 'Database seeded successfully' });
+    } catch (error) {
+        console.error('Database Error:', error);
+        await client.sql`ROLLBACK`;
+        return Response.json({error}, {status: 500});
+    }
+}
