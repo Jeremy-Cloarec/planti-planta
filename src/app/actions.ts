032c64034d5e158d4cc10cf09e820d8f38e588d8
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-02-24 11:48:42 +0100

    refactor: add try catch block

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,9 +1,17 @@
 'use server'
 import { Plant } from "@/app/lib/definitions";
 import { connectionPool as cp } from "@/app/db"
+import { revalidatePath } from "next/cache"
 
-export async function updateStockStore (storePlants: Plant[]) {
-    for (const plant of storePlants) {
-        cp.query(`UPDATE plants SET quantity = quantity - '${plant.quantity}' WHERE id = '${plant.id}'`)
+export async function updateStockStore(storePlants: Plant[]) {
+    try {
+        for (const plant of storePlants) {
+            cp.query(`UPDATE plants SET quantity = quantity - '${plant.quantity}' WHERE id = '${plant.id}'`)
+
+            console.log(`${plant.title} a bien été modifiée`);
+        }
+    } catch (error) {
+        console.error(error)
     }
+    revalidatePath('/post')
 }
