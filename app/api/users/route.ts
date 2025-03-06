bd832c722efab657c9d33722f99176515e1fc119
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-03-06 16:03:49 +0100

    fix: return 401 and not null in GET function

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,9 +5,15 @@ import { getSession } from "@/app/lib/session";
 
 export async function GET() {
     const userSession = await getSession()
-    if (!userSession) return null
-    console.log("userSession");
     
+    if (!userSession) {
+        return NextResponse.json(
+            { error: "Unauthorized" },
+            { status: 401 }
+        );
+    }
+    console.log("userSession");
+
     try {
         const data = await cp.query(`SELECT name, email FROM users WHERE id = (${userSession.userId})`)
         const user = data.rows[0]
