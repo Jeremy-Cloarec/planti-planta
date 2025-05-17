d50fc055e7d5a88337babac1cf0403888c14586b
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-05-17 16:11:19 +0200

    fix: jsx syntaxe on svg attributes

--- app/ui/buttons/ButtonAddToBasket.tsx
+++ app/ui/buttons/ButtonAddToBasket.tsx
@@ -22,7 +22,6 @@ export default function ButtonAddToBasket({ text, plantId, userId, disabled, add
     const queryClient = useQueryClient()
 
     const handleAddToBasket = async () => {
-
         const res = await addPlantToBasket(plantId, userId)
 
         if (!res) {
@@ -55,7 +54,7 @@ export default function ButtonAddToBasket({ text, plantId, userId, disabled, add
         if (isPending) return <div className="flex justify-center">
             <svg className="size-5 animate-spin"
                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 22 22" fill="none">
-                <path d="M11 1V5.167M11 16.833V21M21 11H16.834M5.167 11H1M18.209 3.792L15.292 6.709M6.708 15.292L3.791 18.208M18.209 18.208L15.292 15.292M6.708 6.709L3.791 3.792" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M11 1V5.167M11 16.833V21M21 11H16.834M5.167 11H1M18.209 3.792L15.292 6.709M6.708 15.292L3.791 18.208M18.209 18.208L15.292 15.292M6.708 6.709L3.791 3.792" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
         </div>
         return text
