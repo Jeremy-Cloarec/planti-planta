9bfa705127e74570db2c7336750afd335f923a1f
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-06-14 15:43:55 +0200

    style: basket desktop vertsion

--- app/panier/page.tsx
+++ app/panier/page.tsx
@@ -7,6 +7,7 @@ import { useQuery } from "@tanstack/react-query"
 import { formatedUrl } from "../utils/utils"
 import { cabinBold } from "../ui/fonts"
 import ButtonOrder from "../ui/buttons/ButtonOrder"
+import { Footer } from "../ui/Footer"
 
 export default function Basket() {
     const userQuery = useQuery<User>({
@@ -49,14 +50,17 @@ export default function Basket() {
     return (
         <>
             <Nav numberOfPlants={countNav.data} />
-            <main className="w-full flex-1 pt-[72px] flex flex-col gap-4">
+            <main className="w-full flex-1 pt-[72px] flex flex-col gap-4 max-w-screen-lg">
                 <div className="flex items-center justify-between my-6 px-3">
                     <h1>Votre panier</h1>
                     {totalPrice.data > 0 && <p className={`${cabinBold.className} text-3xl`}>{totalPrice.data} €</p>}
                 </div>
                 {plants.length > 0 ? (
-                    <div className="flex flex-col gap-6 flex-1 justify-between">
-                        <ul className="flex flex-col gap-4 px-3">
+                    <div className="flex flex-col gap-6 flex-1 justify-between
+                    md:flex-row md:items-start md:px-3
+                    ">
+                        <ul className="flex flex-col gap-4 px-3 
+                        md:flex-[3] md:px-0 md:gap-6">
                             {plants.map(plant => (
                                 <li
                                     key={plant.id}
@@ -82,7 +86,8 @@ export default function Basket() {
                                 </li>
                             ))}
                         </ul>
-                        <div className="bg-slate-100 px-3 pt-3 pb-16 flex flex-col gap-3">
+                        <div className="relative bg-slate-100 px-3 pt-3 pb-16 flex flex-col gap-3 
+                        md:flex-[2] md:sticky md:top-[72px] md:p-3">
                             <h2>Récapitulatif de la commande</h2>
                             <div className="flex items-center justify-between text-dark2">
                                 <p>Sous total</p>
@@ -96,21 +101,27 @@ export default function Basket() {
                                 <p>Frais de livraison</p>
                                 <p className="text-lime-600">Gratuit</p>
                             </div>
-                            <div className={`flex items-center justify-between py-3 ${cabinBold.className} border border-t-dark2`}>
+                            <div className={`flex items-center justify-between py-3 ${cabinBold.className}  border-t border-y-slate-400`}>
                                 <p>TOTAL</p>
                                 <p> {totalPrice.data} €</p>
                             </div>
+                            <div className="p-3 fixed bottom-0 left-0 w-full bg-white shadow-[0_2px_12px_rgba(0,0,0,0.1)]
+                            md:relative md:p-0 md:shadow-none
+                            ">
+                                <ButtonOrder
+                                    text="Passer la commande"
+                                />
+                            </div>
                         </div>
-                        <div className="p-3 fixed bottom-0 w-full bg-white shadow-[0_2px_12px_rgba(0,0,0,0.1)]">
-                            <ButtonOrder
-                                text="Passer la commande"
-                            />
-                        </div>
+
                     </div>
                 ) : (
                     <p className="p-3">Vous n&apos; avez pas encore de plantes dans votre panier</p>
                 )}
             </main>
+            <div className="hidden md:block w-full">
+                <Footer />
+            </div>
         </>
 
     )
