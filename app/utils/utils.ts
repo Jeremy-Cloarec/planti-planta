91cbf4ba81feab9ac0a39a8271afceebb14ad380
Jeremy-Cloarec <jeremycloarec@msn.com>
2025-07-01 12:03:45 +0200

    refactor: remove `urls.baseUrl` and unused `getBaseUrl` function

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,10 +1,1 @@
 export const  formatedUrl = (title:string) =>  title.toLowerCase().split(" ").join("_")
-
-const getBaseUrl = () => {
-    if (process.env.NEXT_PUBLIC_SITE_URL) return `https://${process.env.NEXT_PUBLIC_SITE_URL}`
-    return `http://localhost:${process.env.PORT || 3000}`
-}
-
-export const urls = {
-    baseUrl: getBaseUrl(),
-}
