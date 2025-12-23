733a21a206d8193eb55d90fab48b165a0e68a642
Jerem <jeremycloarec@msn.com>
2025-12-23 21:11:08 +0100

    feat: add userStripeId in db  in checkoutpayment process

--- app/api/stripe/create-checkout-session/route.ts
+++ app/api/stripe/create-checkout-session/route.ts
@@ -1,12 +1,49 @@
+import { auth } from "@/app/lib/auth";
 import { PlantInBasket } from "@/app/lib/definitions";
 import stripe from "@/app/lib/stripe";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { connectionPool as cp } from "app/db";
+
 
 export async function POST(req: NextRequest) {
-    const data: PlantInBasket[] = await req.json();
+    const dataPlants: PlantInBasket[] = await req.json();
 
-    const plantsPayment = data.map(plant => ({
+    //Create stripe client
+    const cookieStore = await cookies();
+
+    const session = await auth.api.getSession({
+        headers: {
+            cookie: cookieStore.toString(),
+        },
+    });
+
+    let stripeCustomerId: string | undefined;
+
+    if (session?.user?.email) {
+        // Check if user have a stripeCustomerId
+        stripeCustomerId = ((await cp.query(`SELECT "stripeCustomerId" FROM "user" WHERE "id"=$1`, [session.user.id])).rows[0]).stripeCustomerId;
+
+        console.log(stripeCustomerId);
+
+        if (!stripeCustomerId) {
+            const customer = await stripe.customers.create({
+                name: session.user.name,
+                email: session.user.email,
+                metadata: {
+                    userId: session.user.id,
+                },
+            });
+
+            stripeCustomerId = customer.id;
+
+            await cp.query(`UPDATE "user" SET "stripeCustomerId" = $1 WHERE "id" = $2`, [stripeCustomerId, session.user.id]);
+        }
+    }
+
+    console.log(stripeCustomerId);
+
+    const plantsPayment = dataPlants.map(plant => ({
         price_data: {
             currency: 'eur',
             product_data: {
@@ -15,7 +52,7 @@ export async function POST(req: NextRequest) {
             unit_amount: plant.unitPrice * 100,
         },
         quantity: plant.basketQuantity,
-    }))      
+    }))
 
     try {
         const session = await stripe.checkout.sessions.create({
@@ -27,12 +64,12 @@ export async function POST(req: NextRequest) {
         })
 
         const cookieStore = await cookies();
-        if(!session.client_secret) {
+        if (!session.client_secret) {
             throw new Error('Failed to create secret client');
         };
 
         cookieStore.set('secretClientStripe', session.client_secret)
-        
+
         return NextResponse.json({ status: session.status });
 
     } catch (error) {
