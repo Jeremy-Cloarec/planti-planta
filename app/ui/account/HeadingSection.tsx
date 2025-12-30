7afca5cb276cba1f70c4eb767442ed9dec3d10de
Jerem <jeremycloarec@msn.com>
2025-12-30 10:19:27 +0100

    fix: change <Tag> to Component

--- app/ui/account/HeadingSection.tsx
+++ app/ui/account/HeadingSection.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { cabinBold } from "@/app/ui/fonts"
 import ButtonChangeInfo from "@/app/ui/buttons/ButtonChangeInfo"
 
@@ -5,15 +6,16 @@ type HeadingSectionProps = {
     text: string
     onClick?: () => void
     textButton: string
-    as?: keyof JSX.IntrinsicElements
+    as?: React.ElementType
     style?: string
 
 };
 
 export default function HeadingSection({ text, onClick, textButton, as: Tag = "h2", style }: HeadingSectionProps) {
+    const Component = Tag as React.ElementType;
     return (
         <div className="flex items-center justify-between">
-            <Tag className={`${cabinBold.className} ${style}`}>{text}</Tag>
+            <Component className={`${cabinBold.className} ${style}`}>{text}</Component>
             <ButtonChangeInfo onClick={onClick} textButton={textButton} />
         </div>
     )
